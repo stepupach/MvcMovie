28c5522de8950f8a7459ec64018745d526f1551f
stepu <stepu@STEPUPACH>
2024-09-21 16:15:55 +0300

    Добавьте файлы проекта.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; } // ? после string указывает на то, что свойство может быть обнуляемым
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
